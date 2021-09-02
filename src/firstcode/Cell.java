f89e98fdc7b33195a11668375b97d69e71ea3e82
mayans16 <35878814+mayans16@users.noreply.github.com>
2021-09-01 22:01:08 -0700

    Added cell class

---
+++ src/firstcode/Cell.java
@@ -0,0 +1,78 @@
+package firstcode;
+
+public class Cell {
+    private int strength;
+    private int x;
+    private int y;
+    private int id;
+
+    public Cell(){
+        this.strength = 0;
+        this.id = 0;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Cell(int strength, int x, int y, int id){
+        this.setStrength(strength);
+        this.setX(x);
+        this.setY(y);
+        this.setId(id);
+    }
+
+    public int getStrength(){
+        return this.strength;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setStrength(int strength){
+        if (strength > 0){
+            this.strength = strength;
+        }
+        else{
+            this.strength = 0;
+        }
+    }
+
+    public void setX(int x){
+        if (x > 0){
+            this.x = x;
+        }
+        else{
+            this.x = 0;
+        }
+    }
+
+    public void setY(int y){
+        if (y > 0){
+            this.y = y;
+        }
+        else{
+            this.y = 0;
+        }
+    }
+
+    public void setId(int id){
+        if (id > 0){
+            this.id = id;
+        }
+        else{
+            this.id = 0;
+        }
+    }
+
+    public void interactNeighbors(ArrayList<Cell> neighbors){
+
+    }
+}
